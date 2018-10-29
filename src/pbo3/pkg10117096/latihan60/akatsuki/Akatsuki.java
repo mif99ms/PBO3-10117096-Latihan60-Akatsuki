80540ff420d4e437833ab95f91ffaf2a929c0789
mif99ms <msidqi86@gmail.com>
2018-10-29 13:29:50 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan60/akatsuki/Akatsuki.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan60.akatsuki;
+
+/**
+ *
+ * @author 
+ * Nama         : Miftah Muhammad Sidqi
+ * NIM          : 10117096
+ * Kelas        : PBO 3
+ */
+public class Akatsuki {
+      protected String clothes;
+    protected String goals;
+
+    public Akatsuki(String clothes, String goals) {
+        this.clothes = clothes;
+        this.goals = goals;
+    }
+
+    public String getClothes() {
+        return clothes;
+    }
+
+    public String getGoals() {
+        return goals;
+    }
+}
